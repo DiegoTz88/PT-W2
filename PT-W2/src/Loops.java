7ff0173aab06ca70cfb4fa6a9d4a37ac52d2ff5b
Diego Tzompa <tzompad@gmail.com>
2022-05-10 23:13:00 -0600

    Second commit

--- PT-W2/src/Loops.java
+++ PT-W2/src/Loops.java
@@ -3,7 +3,105 @@ public class Loops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//BOOLEAN---------------------------------
+//		static void brokeOrNot(int moneyInBank, int rentDue, int carPayment, int food) {
+//		    // Write your code below
+//		    
+//		    if(moneyInBank > (rentDue + carPayment + food)){
+//		               //Write your conditional statement around these two print statements
+//		       //if statement here
+//		           System.out.println("You have enough money this week.");
+//		    } else {
+//		                //else statement here
+//		           
+//		           System.out.println("You may need to borrow some money.");
+//		       
+//		       //Write your conditional statement around these two print statements
+//		    }
+	//-------------------------------------------------	
+//	    // Create your if statement here 
+//	    if(personOneAge > personTwoAge && personOneAge > personThreeAge){
+//		    System.out.println("Person one is older."); //Keep this statement 
+//	    } else if (personTwoAge > personOneAge && personTwoAge > personThreeAge){
+//		// Create your else if statement here 
+//		    System.out.println("Person two is older."); //Keep this statement
+//	    } else {
+//	    // Create your else statement here
+//		    System.out.println("Person three is older."); //Keep this statement 
+//	    }
+	//--------------------------------------------------
+//		if (sundayTemp > mondayTemp && sundayTemp > tuesdayTemp) {
+//		    System.out.println("Sunday");
+//		} else if (mondayTemp > sundayTemp && mondayTemp > tuesdayTemp) {
+//		    System.out.println("Monday");
+//		} else {
+//		    System.out.println("Tuesday");
+//		}
+		
+		//CONDITIONALS---------------------------------------
+//		if(numericGrade >= 90){
+//		    System.out.println('A');
+//		} else if (numericGrade >= 80 && numericGrade < 90) {
+//		    System.out.println('B');
+//		} else if (numericGrade >= 70 && numericGrade < 80) {
+//		    System.out.println('C');
+//		} else if (numericGrade >= 60 && numericGrade < 70) {
+//		    System.out.println('D');
+//		} else if (numericGrade < 60) {
+//		    System.out.println('F');
+//		}
+//		
+	//-------------------------------------------------
+//        if(num1 >= num2){
+//            if(num3 >= num4){
+//                System.out.println("both statements are true");
+//            }
+//            System.out.println("program ended");
+//        } else {
+//            System.out.println("program ended");
+//        }
+	//--------------------------------------------------
+//	       if(x > 0 && y > 0){
+//	           System.out.println("The point is in quadrant 1.");
+//	       } else if (x < 0 && y > 0){
+//	           System.out.println("The point is in quadrant 2.");
+//	       } else if (x < 0 && y < 0){
+//	           System.out.println("The point is in quadrant 3.");
+//	       } else if (x > 0 && y < 0){
+//	           System.out.println("The point is in quadrant 4.");
+//	       } else {
+//	           System.out.println("The point is resting on the x or y axis.");
+//	       }
+		//LOOPS-------------------------------------------------
+//        int i = 0;
+//        do{
+//            numToAdd += numToAdd;
+//            i++;
+//        }while(i<howManyTimes);
+//        System.out.println(numToAdd);
+//  }
+	//----------------------------------------------------
+//        if(userValue > 0){
+//            for(int i = 1; i < 100; i += userValue){
+//                System.out.println(i);
+//            }
+//        } else {
+//            System.out.println("Your number needs to be greater than 0.");
+//        }
+	//-----------------------------------------------------
+//		if(wordLength >= 5){
+//		    int i = 1;
+//		    do{
+//		        System.out.println(userWord);
+//		        i++;
+//		    }while(i<=counter);
+//		} else {
+//		    System.out.println(userWord);
+//		}
+//        
+//        
+        
 	}
 
 }
